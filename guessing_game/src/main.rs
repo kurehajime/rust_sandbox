3df909425247175127e0ebc18ac9a0aa8147dde0
kurehajime <xiidec@gmail.com>
2021-12-31 05:55:53 +0000

    数当てゲーム

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,27 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("数当てゲーム❗");
-    println!("数字を入れてね");
 
-    let mut guess = String::new();
-
-    io::stdin().read_line(&mut guess)
-        .expect("読み取り失敗！");
-
-    println!("あなたの予想は {}",guess);
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    loop {
+        println!("数字を入れてね");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("読み取り失敗！");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("あなたの予想は {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("小さいよ"),
+            Ordering::Equal => {
+                println!("ちょうど");
+                break;
+            }
+            Ordering::Greater => println!("大きいよ"),
+        }
+    }
 }
